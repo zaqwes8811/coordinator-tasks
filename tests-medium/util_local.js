9dedb566ddcc104920cb7da0575d5962ad0221ee
bb <igor.a.lugansky@gmail.com>
2013-10-26 19:50:23 +0400

    save

---
+++ tests-medium/util_local.js
@@ -0,0 +1,11 @@
+/**
+ * New node file
+ */
+
+function extend(Child, Parent) {
+    var F = function() { };
+    F.prototype = Parent.prototype;
+    Child.prototype = new F();
+    Child.prototype.constructor = Child;
+    Child.superclass = Parent.prototype;
+}
