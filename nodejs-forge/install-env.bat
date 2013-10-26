e216cc21903083b28e0c691bd863ff0ac8c389da
bb <igor.a.lugansky@gmail.com>
2013-10-26 14:02:02 +0400

    add

---
+++ nodejs-forge/install-env.bat
@@ -0,0 +1,10 @@
+rem need install Node.js and add path
+rem call npm config set proxy http://172.16.11.77:8080
+rem call npm config set https-proxy http://172.16.11.77:8080
+
+rem dependences
+call npm install mocha
+call npm install request
+call npm install underscore
+call npm install chai
+call npm install net-snmp
