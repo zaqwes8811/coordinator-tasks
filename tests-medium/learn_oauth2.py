0442095ce0a9b1ce2710eee3233a17611ebde5e9
bb <igor.a.lugansky@gmail.com>
2013-10-27 13:03:24 +0400

    upload file to GDrive.

--- tests-medium/learn_oauth2.py
+++ tests-medium/learn_oauth2.py
@@ -44,3 +44,36 @@ http = credentials.authorize(http)
 drive_service = build('drive', 'v2', http=http)
 
 # Как понял можно работать с файлами!
+
+# Insert a file
+media_body = MediaFileUpload('bigfile.zip', mimetype='application/octet-stream', chunksize=1024*256, resumable=True)
+body = {
+    'title': 'bigfile.zip',
+    'description': 'Big File',
+    'mimeType': 'application/octet-stream'
+}
+
+retries = 0
+request = drive_service.files().insert(body=body, media_body=media_body)
+response = None
+while response is None:
+    try:
+            print http.request.credentials.access_token
+            status, response = request.next_chunk()
+            if status:
+                    print "Uploaded %.2f%%" % (status.progress() * 100)
+                    retries = 0
+    except errors.HttpError, e:
+            if e.resp.status == 404:
+                    print "Error 404! Aborting."
+                    exit()
+            else:
+                    if retries > 10:
+                            print "Retries limit exceeded! Aborting."
+                            exit()
+                    else:
+                            retries += 1
+                            time.sleep(2**retries)
+                            print "Error (%d)... retrying." % e.resp.status
+                            continue
+print "Upload Complete!"
