a7f80cdae66d3d4662055fda067a1c8863cb1327
bb <bb@gmail.com>
2013-11-18 19:34:29 +0400

    save and push

--- jcoordinator/src/MainTest.java
+++ jcoordinator/src/MainTest.java
@@ -1,5 +1,10 @@
 import junit.framework.TestCase;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.RepositoryService;
 import org.junit.Test;
+
+// http://stackoverflow.com/questions/11326045/github-java-api-search-repos
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,8 +14,19 @@ import org.junit.Test;
  * To change this template use File | Settings | File Templates.
  */
 public class MainTest extends TestCase{
+    private final  String userName = "zaqwes8811";
     @Test
-    public void testMultiply() {
+    public void testMultiply() throws Exception {
+        RepositoryService service = new RepositoryService();
+        for (Repository repo : service.getRepositories(userName))
+            System.out.println(repo.getName());
+    }
 
+    @Test
+    public void testIssuesAccess() throws Exception {
+        IssueService issueService = new IssueService();
+        issueService.getIssues(userName);
+        for (Repository repo : )
+            System.out.println(repo.getName());
     }
 }
