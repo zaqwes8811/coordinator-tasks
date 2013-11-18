e505ed9226bf1b9107c458d2e7994413fa2613ef
bb <bb@gmail.com>
2013-11-18 19:46:00 +0400

    Need auth.

--- jcoordinator/src/MainTest.java
+++ jcoordinator/src/MainTest.java
@@ -1,5 +1,6 @@
 import junit.framework.TestCase;
 import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.RequestException;
 import org.eclipse.egit.github.core.service.IssueService;
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.junit.Test;
@@ -18,15 +19,20 @@ public class MainTest extends TestCase{
     @Test
     public void testMultiply() throws Exception {
         RepositoryService service = new RepositoryService();
-        for (Repository repo : service.getRepositories(userName))
+        for (Repository repo : service.getRepositories(userName)) {
             System.out.println(repo.getName());
+            try {
+                System.out.println(new IssueService().getIssue(repo, 1).getAssignee());
+            } catch (RequestException e) {
+                System.out.println("No issues");
+            }
+        }
     }
 
     @Test
     public void testIssuesAccess() throws Exception {
-        IssueService issueService = new IssueService();
-        issueService.getIssues(userName);
-        for (Repository repo : )
-            System.out.println(repo.getName());
+        // Так а куда передать чей репозиторий?
+        //IssueService issueService = new IssueService();
+        //issueService.getIssues();
     }
 }
