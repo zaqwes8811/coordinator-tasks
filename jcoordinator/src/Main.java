721faf3910168c00ba34ef837a997980b1dbf3f2
bb <bb@gmail.com>
2013-11-18 19:02:37 +0400

    save and push

---
+++ jcoordinator/src/Main.java
@@ -0,0 +1,7 @@
+// http://www.vogella.com/articles/JUnit/article.html
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ jcoordinator/src/MainTest.java
@@ -0,0 +1,16 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zaqwes_user
+ * Date: 18.11.13
+ * Time: 18:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainTest extends TestCase{
+    @Test
+    public void testMultiply() {
+
+    }
+}
