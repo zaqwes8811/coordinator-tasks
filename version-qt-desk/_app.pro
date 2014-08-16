89f84c9853fcde18f0cf19bcc9bc94a3cafd27f8
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-08-16 23:01:20 +0400

    Begin build min version.

---
+++ version-qt-desk/_app.pro
@@ -0,0 +1,27 @@
+# http://stackoverflow.com/questions/2580934/how-to-specify-different-debug-release-output-directories-in-qmake-pro-file
+
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+TARGET = a.app
+
+BOOST=/home/zaqwes/work/third_party/boost_1_55_0
+_3RDPARTY=../3rdparty
+
+# -std=c++11 -pg
+QMAKE_CXXFLAGS += -O2  -Wall -Wno-deprecated 
+
+SOURCES += app.cc 
+#\
+#  ../../third_party/gmock-1.6.0/fused-src/gmock-gtest-all.cc \
+#  ../launcher.cc
+
+INCLUDEPATH += $$BOOST $$_3RDPARTY/loki-0.1.7/include
+#../../third_party/gmock-1.6.0/fused-src/
+
+LIBS += -lpthread -lrt -ltbb -lpq -lpqxx
+
+OBJECTS_DIR = obj
+
---
+++ version-qt-desk/app.cc
@@ -0,0 +1,177 @@
+#include <typeinfo>
+
+// C
+#include <cassert>
+
+// C++
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+// 3rdparty::Common
+#include <loki/ScopeGuard.h>
+
+// 3rdparty::Special
+#include <pqxx/pqxx> 
+
+using namespace std;
+using namespace pqxx;
+using namespace Loki;
+
+namespace {
+const string gTableName = "tasks";
+
+class Task {
+ 
+public:
+  
+  
+private:
+  // Уникальный для каждой задачи
+  int primary_key_;  // нужно какое-то не активное
+  
+  // Джоел первая.
+  // Feature - пока нет
+  // Task name
+  string task_name_;
+  
+  // Priority
+  int priority;
+  
+  // Origin est.
+  // Current est.
+  // Elapsed
+  // Remain
+  
+  std::set<std::string> tags_;
+};
+
+namespace dal {
+void rm_table(connection* C) {
+  // Если таблицы нет, то просто ничего не происходит.
+  string sql = string("drop table " + gTableName + ";");
+  
+  // создаем транзакционный объект
+  work W(*C);
+  
+  cout << "Dropping" << endl;
+  
+  // Exec
+  W.exec(sql);
+  W.commit();
+}
+
+void run_transaction(const string& sql, /*const*/ connection& C)  
+{
+  work W(C);
+  W.exec(sql);
+  W.commit();
+}
+
+void create_task_table(const string& table_name) {
+  
+}
+
+void drop_task_table() {
+  
+}
+
+// Делать один репозиторий не советуют
+// TODO: Может DI какой сделать, или все равно?
+class TaskLifetimeQueries {
+public:
+  void persist(const Task& task) const {
+    
+  }
+  
+  // Назначет id!
+  Task& create(Task& task) const { 
+    	
+    /*
+    {
+      // Insert
+      // http://stackoverflow.com/questions/7718585/set-auto-increment-primary-key-in-postgresql
+      //string sql = "INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) " \
+      //"VALUES (1, 'Paul', 32, 'Calif', 200000.00); ";
+      
+      string sql = "INSERT INTO " + gTableName + " (NAME, AGE, ADDRESS, SALARY) " \
+      "VALUES ('Paul', 32, 'Calif', 200000.00); " \
+      "VALUES ('Djudy', 32, 'Calif', 900000.00); ";
+      
+      run_transaction(sql, C);
+    }
+    */
+    return task; 
+  }
+};
+
+}  // namespace..
+}  // namespace..
+
+int main() {
+  using namespace dal;
+  try {
+  // construction is safe? 
+    connection C("dbname=mydb user=postgres password=postgres hostaddr=127.0.0.1 port=5432");
+    {
+      // DANGER: Рефакторинг мог быть не эквивалентным, но в api сказано, что закрывает только откр. соед.
+      if (!C.is_open()) {
+	//guard.Dismiss();  // not need it
+	throw runtime_error("Can't open database");
+      }
+      
+      ScopeGuard guard = MakeObjGuard(C, &connection::disconnect);
+      
+      /// Work
+      {
+	//ScopeGuard table = MakeGuard(&rm_table, &C);  // а если не создасться? Тут похоже все равно.
+	
+	{ // Create table 
+	  // TODO: create only if not exist - с исключением не катит.
+	  // TODO: а кто вообще создает таблицы? Всегда ли пользователь?
+	  //
+	  // TODO: как безболезненной сделать изменение схемы хранения? Как делать рефакторинг?
+	  string sql = string("CREATE TABLE ") +  
+	    // http://stackoverflow.com/questions/1766046/postgresql-create-table-if-not-exists
+	    "IF NOT EXISTS "+ // v9.1 >=
+	    gTableName +
+	    "(" \
+	    // http://www.tutorialspoint.com/postgresql/postgresql_using_autoincrement.htm
+	    //"ID INT PRIMARY KEY NOT NULL," \  // own control
+	    "ID SERIAL PRIMARY KEY NOT NULL," \
+	    "NAME  TEXT NOT NULL, " \
+	    "AGE INT NOT NULL," \
+	    "ADDRESS CHAR(50),"\
+	    "SALARY REAL);";
+	  
+	  // создаем транзакционный объект
+	  run_transaction(sql, C);
+	}
+	
+	// Лучше здесь - раз дошли до сюда, то таблица создана
+	ScopeGuard table = MakeGuard(&rm_table, &C);
+
+      }
+    }
+
+    // POINT - disconnected
+    assert(!C.is_open());
+  } catch (const pqxx::undefined_table& e) {
+    // Нет таблицы
+    cerr << typeid(e).name() << endl << e.what() << endl;
+  } catch (const pqxx::unique_violation& e) {
+    // Не уникальный ключ при вствке
+    cerr << typeid(e).name() << endl << e.what() << endl;
+  } catch (const pqxx::sql_error& e) {
+    // Кажется ошибка в синтаксисе
+    cerr << typeid(e).name() << endl << e.what() << endl;
+  } catch (const std::exception& e) {
+    cerr << "UNCATCHED" << endl;
+    cerr << typeid(e).name() << endl;
+    cerr << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
+}
