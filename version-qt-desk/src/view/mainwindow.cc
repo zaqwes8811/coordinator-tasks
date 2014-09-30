dc6b70ab5db21243bf43a667f69769eb14f16d40
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-09-30 14:57:37 +0400

    save

--- version-qt-desk/src/view/mainwindow.cc
+++ version-qt-desk/src/view/mainwindow.cc
@@ -1,13 +1,3 @@
-// selection problem:
-//  http://stackoverflow.com/questions/11816272/qtablewidget-cellactivated-signal-not-working
-//
-// How check edited state:
-//   http://www.qtcentre.org/threads/7976-How-to-know-when-a-cell-in-a-QTableWidget-has-been-edited
-//   http://qt-project.org/forums/viewthread/31372
-//
-// Internationalization:
-//  http://stackoverflow.com/questions/4214369/how-to-convert-qstring-to-stdstring
-
 #include "top/config.h"
 
 // App
--- version-qt-desk/src/view/mainwindow.h
+++ version-qt-desk/src/view/mainwindow.h
@@ -13,6 +13,19 @@ class MainWindow;
 }
 
 class QTableWidgetCheckEdited : public QTableWidget {
+  // TableWidget troubles list:
+  //
+  // http://qt-project.org/forums/viewthread/7769 - Move column
+  //
+  // selection problem:
+  //  http://stackoverflow.com/questions/11816272/qtablewidget-cellactivated-signal-not-working
+  //
+  // How check edited state:
+  //   http://www.qtcentre.org/threads/7976-How-to-know-when-a-cell-in-a-QTableWidget-has-been-edited
+  //   http://qt-project.org/forums/viewthread/31372
+  //
+  // Internationalization:
+  //  http://stackoverflow.com/questions/4214369/how-to-convert-qstring-to-stdstring
 public:
   explicit QTableWidgetCheckEdited(QWidget *parent = 0)
     : QTableWidget(parent) {}
