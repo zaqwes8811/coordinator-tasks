e15b7edb13b28f6898b33392c8c0a2e786827af4
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-17 16:39:50 +0300

    first alembic action

---
+++ rich-client-impl/python-desk/python_pq_bridge/alembic/versions/1f70f92c54bb_create_account_table.py
@@ -0,0 +1,22 @@
+"""create account table
+
+Revision ID: 1f70f92c54bb
+Revises: 74d89c0efb3
+Create Date: 2014-11-17 16:34:23.859000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1f70f92c54bb'
+down_revision = '74d89c0efb3'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
