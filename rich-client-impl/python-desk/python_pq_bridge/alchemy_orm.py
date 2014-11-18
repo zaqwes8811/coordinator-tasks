9963c0d1adc6c9718378f49efab7de216a00835e
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-18 15:15:08 +0300

    migrate and make first query

--- rich-client-impl/python-desk/python_pq_bridge/alchemy_orm.py
+++ rich-client-impl/python-desk/python_pq_bridge/alchemy_orm.py
@@ -8,6 +8,7 @@ from contextlib import contextmanager
 # relations
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy import and_
 
 Base = declarative_base()  # it's class?!
 
@@ -24,7 +25,7 @@ def build_engine():
     # , connect_args={'time_zone': '+00:00'} - failed
 
     # now connected
-    engine.echo = True
+    #engine.echo = True
     return engine
 
 
@@ -54,7 +55,23 @@ class TaskEntity(Base):
     priority = Column(String)
     done = Column(Boolean)
 
+    tags = relationship('Tag', secondary='task_tag_link')
 
+class Tag(Base):
+    __tablename__ = 'tags'
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String)
+    color = Column(String)
+
+    tasks = relationship(TaskEntity, secondary='task_tag_link')
+
+
+class TaskTagLink(Base):
+    __tablename__ = 'task_tag_link'
+    # !! naming is strong here
+    task_entity_id = Column(Integer, ForeignKey('task_entity.id'), primary_key=True)
+    tags_id = Column(Integer, ForeignKey('tags.id'), primary_key=True)
 
 def main():
     engine = build_engine()
@@ -62,8 +79,11 @@ def main():
     Base.metadata.create_all(engine)
 
     with session_scope(Session) as session:
-        for instance in session.query(TaskEntity):
-            print instance.priority, instance.task_name, instance.feature
+        # add task and tag
+
+        for instance in session.query(TaskEntity)\
+                .filter(and_(TaskEntity.task_name.match("*VCard*"), TaskEntity.done == False)):
+            print instance.tags, instance.priority, instance.task_name, instance.feature
 
 
 if __name__ == '__main__':
