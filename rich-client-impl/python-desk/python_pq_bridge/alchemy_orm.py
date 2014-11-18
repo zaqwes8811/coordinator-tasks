e06a9659e89fcd4459b52fcf0de0fbaa7e76cb85
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-18 16:42:11 +0300

    m2m dal schema done

--- rich-client-impl/python-desk/python_pq_bridge/alchemy_orm.py
+++ rich-client-impl/python-desk/python_pq_bridge/alchemy_orm.py
@@ -10,6 +10,9 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import and_
 
+from sqlalchemy.orm.exc import MultipleResultsFound
+from sqlalchemy.orm.exc import NoResultFound
+
 Base = declarative_base()  # it's class?!
 
 
@@ -24,8 +27,9 @@ def build_engine():
         db='postgres'))
     # , connect_args={'time_zone': '+00:00'} - failed
 
-    # now connected
+    # tune
     #engine.echo = True
+    #engine.  # хотел отформатировать, но так можно в Hibernate non here
     return engine
 
 
@@ -42,8 +46,9 @@ def session_scope(Session):
     finally:
         session.close()
 
+
 # Migration
-#  !! http://stackoverflow.com/questions/4209705/is-it-worth-using-sqlalchemy-migrate
+# !! http://stackoverflow.com/questions/4209705/is-it-worth-using-sqlalchemy-migrate
 #   https://sqlalchemy-migrate.readthedocs.org/en/latest/
 class TaskEntity(Base):
     __tablename__ = 'task_entity'
@@ -55,7 +60,8 @@ class TaskEntity(Base):
     priority = Column(String)
     done = Column(Boolean)
 
-    tags = relationship('Tag', secondary='task_tag_link')
+    tags = relationship('Tag', secondary='task_tag_link', backref='task_entity')
+
 
 class Tag(Base):
     __tablename__ = 'tags'
@@ -73,17 +79,44 @@ class TaskTagLink(Base):
     task_entity_id = Column(Integer, ForeignKey('task_entity.id'), primary_key=True)
     tags_id = Column(Integer, ForeignKey('tags.id'), primary_key=True)
 
+
 def main():
     engine = build_engine()
     Session = sessionmaker(bind=engine)  # it's class!
     Base.metadata.create_all(engine)
 
+    tag_name = 'project'
     with session_scope(Session) as session:
         # add task and tag
 
-        for instance in session.query(TaskEntity)\
-                .filter(and_(TaskEntity.task_name.match("*VCard*"), TaskEntity.done == False)):
+        tag = None
+        try:
+            #session.query(Tag).filter(Tag.name.has(tag_name))  # !(this is it)
+            tag = session.query(Tag).filter(Tag.name == tag_name).one()
+        except NoResultFound, e:
+            tag = Tag(name=tag_name)
+            session.add(tag)
+
+        except MultipleResultsFound, e:
+            raise  # FIXME: make own
+
+        for instance in session.query(TaskEntity) \
+                .filter(
+                and_(TaskEntity.task_name.match("*VCard*"),
+                     TaskEntity.done == False)):
+            tag.tasks.append(instance)
+            instance.tags = [tag]
             print instance.tags, instance.priority, instance.task_name, instance.feature
+
+    # Select by tag
+    # http://stackoverflow.com/questions/4679846/several-many-to-many-table-joins-with-sqlalchemy
+    # http://www.pythoncentral.io/sqlalchemy-orm-examples/
+    # http://www.lornajane.net/posts/2011/inner-vs-outer-joins-on-a-many-to-many-relationship
+    with session_scope(Session) as session:
+        # just Tag no way
+        # "knows how to join between User and Address because there’s only one foreign key between "
+        for instance in session.query(TaskEntity).join(Tag, TaskEntity.tags).filter(Tag.name == tag_name).all():
+            print instance.task_name
 
 
 if __name__ == '__main__':
