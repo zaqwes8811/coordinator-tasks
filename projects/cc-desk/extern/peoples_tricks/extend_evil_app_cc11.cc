24472e04e18ceafe10d67f7702b2daa29fc58f6e
zaqwes <igor.a.lugansky@gmail.com>
2015-03-16 21:53:59 +0300

    save

--- projects/cc-desk/extern/peoples_tricks/extend_evil_app_cc11.cc
+++ projects/cc-desk/extern/peoples_tricks/extend_evil_app_cc11.cc
@@ -1,97 +1,56 @@
+#include <gtest/gtest.h>
+//#include <boost/shared_ptr.hpp>
+//#include <boost/make_shared.hpp>
+
 #include <vector>
 #include <iostream>
 #include <string>
 #include <memory>
 #include <algorithm>
 
-#include <typeinfo>  // не хотелось бы включать, но похоже нужно
-
-#include <gtest/gtest.h>
-#include <boost/shared_ptr.hpp>
-#include <boost/make_shared.hpp>
-
 using std::ostream;
 using std::string;
 using std::endl;
 using std::cout;
 using std::vector;
-
-// C++11
 using std::move;
 
 namespace ps_sample_shared_extend {
-// Если большие объекты, но почему так работает? Почему не копий?
-template<typename T>
-void draw(const T& x, ostream& out, size_t position)  // object_t -> int and move here
-{ out << string(position, ' ') << x << endl; }
-
-
-// TODO: Whay efficient?
 class object_t {
 public:
   template<typename T>
-  // make_ptr
-  object_t(const T& x) : self_(boost::make_shared<model<T>>(move(x))) {}
+  object_t(const T& x) : self_(std::make_shared<model<T>>(move(x)))
+  { }
 
   friend void draw(const object_t &x, ostream &out, size_t position)
   { x.self_->draw_(out, position); }
-
-  friend int id(const object_t &x);
-  //{ return x.self_->getId(); }
 
 private:
   class concept_t {
   public:
     virtual ~concept_t() = default;
     virtual void draw_(ostream& out, size_t position) const = 0;
-
-    // NVI
-    int getId() const {
-      int id = this->getId_();
-      assert(id >= 0);
-      return id;
-    }
-
-  private:
-    virtual int getId_() const = 0;
   };
 
-  // Объекты выделяются в куче и главный объект в них тоже.
-  // TODO: в чем профит то по скорости?
-  // http://www.cplusplus.com/forum/general/77963/
-  // http://www.barrgroup.com/Embedded-Systems/How-To/Polymorphism-No-Heap-Memory
-  //
-  // "Do your own mem. managment - don't create gerbage for your client to clean up."
-  //
-  // "Returning objects from functions, passing read-only arguments, and passing
-  // rvalues as sink arguments do not require copying"
   template<typename T>
   struct model : concept_t {
     model(const T& x) : data_(move(x)) { }
-    void draw_(ostream& out, size_t position) const
-    {
-      draw(data_, out, position); // она шаблонная
-      //cout << sizeof(*this) << endl;
-      //cout << sizeof(data_) << endl;
+    void draw_(ostream& out, size_t position) const {
+      draw(data_, out, position);
     }
 
     T data_;  // главный вопрос в куче ли? Да - см в Мейсере 35
-  private:
-    virtual int getId_() const {
-      // вызывается просто метод класса - весь полиморфизм спрятан
-      return data_.getId();
-    }
   };
 
-  // std::unique_ptr<int_model_t> self_;
-  //std::
-  boost::shared_ptr<const concept_t> self_;  // ссылки на immutable
+  std::shared_ptr<const concept_t> self_;  // ссылки на immutable
 };
 
-int id(const object_t& x)  // object_t -> int and move here
-{ return x.self_->getId(); }
-
 using document_t = vector<object_t>;  // полиморфизм только через shared_ptrs
+
+template<typename T>
+void draw(const T& x, ostream& out, size_t position)
+{ out << string(position, ' ') << x << endl; }
+
 
 void draw(const document_t&x, ostream &out, size_t position)
 {
@@ -123,8 +82,6 @@ public:
   my_class_t(const my_class_t&) { cout << "copy mc\n"; }
   int arr[10];
 
-//private:
-  int getId() const { return 0; }
 private:
   // FIXME: What the hell?
   static void *operator new (size_t size);
@@ -150,7 +107,6 @@ TEST(EvelPsExtend, App) {
   draw(current(h), cout, 0);
   cout << "-------------" << endl;
   std::reverse(current(h).begin(), current(h).end());
-  int i = id(current(h)[0]);
 
   commit(h);  // сохраняем текущую и ее копируем на верх.
   cout << "-------------" << endl;
@@ -165,5 +121,3 @@ TEST(EvelPsExtend, App) {
   undo(h);
   draw(current(h), cout, 0);
 }
-
-// TODO: add features to class
