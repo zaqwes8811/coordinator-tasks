70a99ede956092909bb723c6e96096ad6eb4de89
zaqwes8811 <igor.a.lugansky@gmail.com>
2016-01-30 23:53:43 +0300

    make qt event executor

--- cc-desk/test_glob_dispatcher.cc
+++ cc-desk/test_glob_dispatcher.cc
@@ -3,15 +3,69 @@
 #include <actors_and_workers/arch.h>
 
 #include <QtWidgets/QApplication>
+#include <QtCore/QEvent>
 
 #include <thread>
 #include <future>
+#include <iostream>
+#include <chrono>
+#include <functional>
+
+using namespace std;
+
+typedef function<void()> Callable;
+static const auto callable_type = QEvent::Type(1001);
+
+class JobEvent : public QEvent
+{
+public:
+	JobEvent( Callable job ) : QEvent( callable_type )
+	{
+		this->job = job;
+	}
+
+	void execute()
+	{
+		job();
+	}
+private:
+	Callable job;
+};
+
+class ExecutorQApplication : public QApplication
+{
+public:
+	ExecutorQApplication( int argc, char **argv ) :
+		QApplication( argc, argv )
+	{
+
+	}
+
+private:
+	void customEvent(QEvent* e)
+	{
+		if( e->type() == callable_type ){
+			auto jobevent = (JobEvent*)e;
+			jobevent->execute();
+		}
+	};
+};
+
+void thread_fn( ExecutorQApplication* app )
+{
+	this_thread::sleep_for( chrono::seconds( 2 ) );
+	JobEvent *e = new JobEvent([] {
+		cout << "hello" << endl;
+	});
+
+	QApplication::postEvent( app, e );
+}
 
 int main(int argc, char **argv)
 {
-	QApplication app (argc, argv);
+	ExecutorQApplication app (argc, argv);
 
-	// fixme: make thread and put message to main loop as Callable
+	thread t( [&app] { thread_fn( &app ); } );
 
 	return app.exec();
 }
