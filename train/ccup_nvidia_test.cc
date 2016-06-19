2ca3928ad1ca34e17dc152415a23a06f5d777898
zaqwes8811 <igor.a.lugansky@gmail.com>
2016-06-19 23:12:48 +0300

    move train code to my-cour

--- train/ccup_nvidia_test.cc
+++
@@ -1,7 +0,0 @@
-
-#include <gtest/gtest.h>
-
-TEST(Nvidia, Pipes) {
-  // http://www.careercup.com/question?id=5753462836953088
-}
-
--- train/ccup_unknown_test.cc
+++
@@ -1,4 +0,0 @@
-
-
-// Queues
-// http://www.careercup.com/question?id=5738324620738560
