775c6558f1bb6f17a635b0ef9c8f733a8fe9a85d
Igor Lugansky <ilugansky@arrival.com>
2022-02-01 22:58:11 +0300

    save

--- cc-desk/doc/design/ReadMe.md
+++
@@ -1,7 +0,0 @@
-
-Compilars
-- GCC 4.7
-- With clang troubles - SEGFAULT
-
-Platforms:
-- Linux/Debian
--- cc-desk/src/scopes.cc
+++
@@ -1,6 +0,0 @@
-#include "heart/config.h"
-
-#include "scopes.h"
-
-
-std::atomic_bool scopes::AppScope::s_isDone{false};
