47d57c755872c138de5608e891869f6e4eed6ce9
zaqwes8811 <igor.a.lugansky@gmail.com>
2023-04-25 12:34:12 +0400

    save

--- my-dockers/yolo3_4_py_docker/bash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-sudo docker start yolo3_4_py_0
-sudo docker exec -it yolo3_4_py_0 bash
--- my-dockers/yolo3_4_py_docker/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-sudo docker stop yolo3_4_py_0 && sudo docker rm yolo3_4_py_0
-sudo docker run \
-	-v /mnt:/mnt \
-	-p 8889:8888 -p 6007:6006 \
-	--name yolo3_4_py_0 \
-	--tmpfs /tmp yolo3_4_py:latest 
